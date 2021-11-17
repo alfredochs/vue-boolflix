0403d0e48da9e4df16371200c0be8bb017afec26
alfredochs <chanta.alfredo@gmail.com>
2021-11-17 15:57:57 +0100

    milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,62 @@
 <template>
-  <div id="app"></div>
+  <div id="app" class="container">
+    <div class="input-group input-group-sm mb-3">
+      <span class="input-group-text" id="inputGroup-sizing-sm">Small</span>
+      <input
+        type="text"
+        class="form-control"
+        aria-label="Sizing example input"
+        aria-describedby="inputGroup-sizing-sm"
+        v-model="query"
+        @keyup.enter="printCards()"
+      />
+    </div>
+
+    <ul>
+      <li v-for="movie in movies" :key="movie.id">
+        {{ movie.title }}
+        ({{ movie.original_title }}) <br />
+        {{ movie.vote_average }}
+        ({{ movie.original_language }})
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      apiKey: "f8519d76cebb62a56eaee41d2d683f32",
+      apiUrl: "https://api.themoviedb.org/3",
+      query: "",
+      movies: [],
+    };
+  },
+  methods: {
+    searchQuery(url, testoDaCercare, dataKey) {
+      axios
+        .get(this.apiUrl + url, {
+          params: {
+            api_key: this.apiKey,
+            query: testoDaCercare,
+          },
+        })
+        .then((resp) => {
+          this[dataKey] = resp.data.results;
+        });
+    },
+    printCards() {
+      this.searchQuery("/search/movie", this.query, "movies");
+    },
+  },
+  mounted() {},
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "~bootstrap/scss/bootstrap";
 </style>
